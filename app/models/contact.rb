9a1c62921a4eea5879807b6c41c1163d2c570cc0
Matheus Oliveira <matheus@Mac-Pro-de-Matheus.local>
2014-09-16 16:48:57 -0300

    clean up contato

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,19 +1,9 @@
 class Contact
-  include ActiveModel::Validations
-  include ActiveModel::Conversion
-  extend ActiveModel::Naming
-
-  #attr_accessor :name, :company, :email, :phone, :comment
+  include ActiveModel::Model
 
   validates :name, presence: true
   validates :email, presence: true, format: { with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/ }
   validates :comment, presence: true, length: { maximum: 800 }
-
-  def initialize(attributes = {})
-    attributes.each do |name, value|
-      send("#{name}=", value)
-    end
-  end
 
   def persisted?
     false
