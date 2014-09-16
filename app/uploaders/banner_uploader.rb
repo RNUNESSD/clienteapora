bacfea62f51c15b2f83a8f91f356ae55e10f0f64
Matheus Oliveira <matheus@Mac-Pro-de-Matheus.local>
2014-09-16 14:37:43 -0300

    dry uploaders

--- app/uploaders/banner_uploader.rb
+++ app/uploaders/banner_uploader.rb
@@ -1,63 +1,12 @@
 # encoding: utf-8
-
-class BannerUploader < CarrierWave::Uploader::Base
-
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+class BannerUploader < BaseUploader
   include CarrierWave::MiniMagick
-
-  # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
-  # include Sprockets::Helpers::RailsHelper
-  # include Sprockets::Helpers::IsolatedHelper
-
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    #"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    "uploads/#{model.class.to_s.underscore.pluralize}/#{mounted_as}"
-  end
-
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
-  # Process files as they are uploaded:
-
-  process :resize_to_model_size
-
 
   version :preview do
     process :resize_to_fill => [200, 140]
   end
 
-  def resize_to_model_size
-    resize_to_fill model.banner_category.image_width, model.banner_category.image_height if model.persisted?
-  end
-
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
   def extension_white_list
     %w(jpg jpeg gif png)
   end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  def filename
-    "#{secure_token}.#{file.extension}" if original_filename.present?
-  end
-
-  protected
-  def secure_token(length=16)
-    var = :"@#{mounted_as}_secure_token"
-    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
-  end
-
 end
--- app/uploaders/default_uploader.rb
+++ app/uploaders/default_uploader.rb
@@ -1,73 +1,12 @@
 # encoding: utf-8
-
-class DefaultUploader < CarrierWave::Uploader::Base
-
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+class DefaultUploader < BaseUploader
   include CarrierWave::MiniMagick
-
-  # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
-  # include Sprockets::Helpers::RailsHelper
-  # include Sprockets::Helpers::IsolatedHelper
-
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    #"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    "uploads/#{model.class.to_s.underscore.pluralize}/#{mounted_as}"
-  end
-
-  #before :cache, :capture_size_before_cache # callback, example here: http://goo.gl/9VGHI
-  #def capture_size_before_cache(new_file)
-  #  if model.upload_width.nil? && model.upload_height.nil?
-  #    model.upload_width, model.upload_height = `identify -format "%wx %h" #{new_file.path}`.split(/x/).map { |dim| dim.to_i }
-  #  end
-  #end
-
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
-  # Process files as they are uploaded:
-
-  # Create different versions of your uploaded files:
-  version :thumb do
-    #process :scale => [50, 50]
-    process :resize_to_fill => [50, 50]
-  end
 
   version :preview do
     process :resize_to_fill => [200, 140]
   end
 
-  version :page do
-    process :resize_to_fill => [206, 157]
-  end
-
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
   def extension_white_list
     %w(jpg jpeg gif png)
   end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  def filename
-    "#{secure_token}.#{file.extension}" if original_filename.present?
-  end
-
-  protected
-  def secure_token(length=16)
-    var = :"@#{mounted_as}_secure_token"
-    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
-  end
-
 end
