9d4a7ab40400e00568c1d4be450cf13dfbc5a98d
Rafael Aquino <programador.pleno.1@corp.agenciaacerte.com>
2017-06-01 10:50:19 -0300

    Remove javascript e adiciona favicon

--- app/assets/javascripts/frontend/functions.js
+++ app/assets/javascripts/frontend/functions.js
@@ -1,25 +1,2 @@
 $(document).ready(function(){
-
-    $(".map").fancybox({
-        'width'             : '80%',
-        'height'            : '80%',
-        'autoScale'         : false,
-        'transitionIn'      : 'none',
-        'transitionOut'     : 'none',
-        'type'              : 'iframe'
-    });
-
-    $(".expand").fancybox({
-        'transitionIn'      : 'none',
-        'transitionOut'     : 'none'
-    });
-
-    //$("#signature_cpf_cnpj").inputmask("{ mask: ["999.999.999-99", "99.999.999/9999-99"]}");
-    $("#contact_phone, #signature_phone").inputmask("(99)9999-9999");
-    $("#signature_birth").inputmask("99/99/9999");
-
-    $('#banner').bxSlider({
-      preloadImages: 'all',
-      pager: false
-    });
 });
