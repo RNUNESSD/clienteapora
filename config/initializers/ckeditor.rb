0a1f939903886b206f8aa01dcc4fed4f0ae385f2
Rafael Aquino <programador.pleno.1@corp.agenciaacerte.com>
2017-08-10 11:05:24 -0300

    Setando o idioma so para pt-BR no ckeditor

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -6,7 +6,9 @@ Ckeditor.setup do |config|
   # available as additional gems.
   require "ckeditor/orm/active_record"
 
-  config.assets_languages = ['pt-br']
+  Ckeditor.setup do |config|
+    config.assets_languages = ['pt-br']
+  end  
 
   # Allowed image file types for upload.
   # Set to nil or [] (empty array) for all file types
